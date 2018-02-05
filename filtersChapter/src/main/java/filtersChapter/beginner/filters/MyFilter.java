1f9e0210f458d80107bab90e0370db7cef337a4f
Nicolò Politi <politi@ariadne.it>
2018-02-05 18:15:41 +0100

    Beginning of filters Chapter

---
+++ filtersChapter/src/main/java/filtersChapter/beginner/filters/MyFilter.java
@@ -0,0 +1,45 @@
+package filtersChapter.beginner.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MyFilter implements Filter {
+
+	private final static Log _log = LogFactory.getLog(MyFilter.class);
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		if(_log.isInfoEnabled()){
+			_log.info("Filter " + this.getClass().getName() + " is starting ...");
+		}
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if(_log.isInfoEnabled()){
+			_log.info("Starting doFilter method of " + this.getClass().getName() + ".");
+		}
+		chain.doFilter(request, response);
+		if(_log.isInfoEnabled()){
+			_log.info("Exiting doFilter method of " + this.getClass().getName() + ".");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		if(_log.isInfoEnabled()){
+			_log.info("... destroying filter " + this.getClass().getName() + ".");
+		}
+	}
+
+}
---
+++ filtersChapter/src/main/java/filtersChapter/beginner/servlets/MySecondServlet.java
@@ -0,0 +1,42 @@
+package filtersChapter.beginner.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MySecondServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private final static Log _log = LogFactory.getLog(MySecondServlet.class);
+	
+	@Override
+	public void init(){
+		if(_log.isInfoEnabled()){
+			_log.info("Servlet " + this.getServletName() + " is starting.");
+		}
+	}
+	
+	@Override
+	public void destroy(){
+		if(_log.isInfoEnabled()){
+			_log.info("Servlet " + this.getServletName() + " is going to be destroyed.");
+		}
+	}
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		if(_log.isInfoEnabled()){
+			_log.info("I'm in doGet method of Servlet " + this.getServletName() + ".");
+		}
+		resp.getWriter().write(this.getServletName());
+		if(_log.isInfoEnabled()){
+			_log.info("Exiting doGet method of Servlet " + this.getServletName() + ".");
+		}
+	}
+}
